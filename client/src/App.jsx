4930c600ef1e3460772df81de9f252b600aa8e91
Daimyo <wasimbessaou@gmail.com>
2024-06-19 11:59:43 -0400

    manueally merged my app jsx and the latest main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
-import IndexPage from './pages/IndexPage'
-import LoginPage from './pages/LoginPage'
-import Layout from './Layout'
-import RegisterPage from './pages/RegisterPage'
-import axios from 'axios'
-import { UserContextProvider } from './UserContext'
-import ProfilePage from './pages/ProfilePage'
-import CarsPage from './pages/CarsPage'
-import CarsFormPage from './pages/CarsFormPage'
-import CarPage from './pages/CarPage'
-import TripsPage from './pages/TripsPage'
-import TripPage from './pages/TripPage'
-import DealsPage from './pages/DealsPage'
-import DealPage from './pages/DealPage'
-import ConfirmEmailPage from './pages/ConfirmEmailPage' // Import the ConfirmEmailPage component
+import { Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+import RegisterPage from './pages/RegisterPage';
+import axios from 'axios';
+import { UserContextProvider } from './UserContext';
+import ProfilePage from './pages/ProfilePage';
+import CarsPage from './pages/CarsPage';
+import CarsFormPage from './pages/CarsFormPage';
+import CarPage from './pages/CarPage';
+import TripsPage from './pages/TripsPage';
+import TripPage from './pages/TripPage';
+import DealsPage from './pages/DealsPage';
+import DealPage from './pages/DealPage';
+import ConfirmEmailPage from './pages/ConfirmEmailPage';
 import TripDetailsPage from './pages/TripDetailsPage'; // Import the new page
-
+// import StripeConnectPage from "./pages/StripeConnectPage";
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
@@ -38,12 +38,13 @@ function App() {
           <Route path='/account/trips/:id/details' element={<TripDetailsPage />} />
           <Route path='/account/deals' element={<DealsPage />} />
           <Route path='/account/deals/:id' element={<DealPage />} />
-          <Route path='/account/trips/:id/details' element={<TripDetailsPage />} />
+          {/* <Route path='/account/trips/:id/details' element={<TripDetailsPage />} /> */}
+          {/* <Route path="/stripe-connect" element={<StripeConnectPage />} /> */}
         </Route>
         <Route path='/confirm-email' element={<ConfirmEmailPage />} />        
       </Routes>
     </UserContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
